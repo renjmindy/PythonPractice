0d15912791263fdf1cd7cc279831eadf2f98d331
Mindy Jen <jencmhep@gmail.com>
2021-10-24 14:40:17 -0400

    update_211024

---
+++ HackerRank_C++/Vectors_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  string oceans[] = {"Pacific", "Atlantic", "Indian", "Arctic", "Southern"};
+  
+  //add code below this line
+  for (int i = 0; i != sizeof(oceans)/sizeof(oceans[0]); i++) {
+    cout << oceans[i] << endl;
+  }
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
