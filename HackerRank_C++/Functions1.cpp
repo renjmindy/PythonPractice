0e7d4f2a860343c90fc6e67392c28e1eea6d5a23
Mindy Jen <jencmhep@gmail.com>
2021-10-28 16:03:44 -0400

    update_211028

---
+++ HackerRank_C++/Functions1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//add code below this line
+
+double GetAvg(double a, double b) {
+  return (a + b)/2;
+}
+
+//add code above this line
+
+int main(int argc, char** argv) {
+  try {
+    double x = stod(argv[1]);
+    double y = stod(argv[2]);
+    cout << GetAvg(x, y) << endl;
+  }
+  catch (invalid_argument& e) {
+    cout << "One or more invalid arguments." << endl;
+  }
+  return 0;
+}
