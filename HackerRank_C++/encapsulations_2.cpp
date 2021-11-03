124a5007b34d4849e9e04aad9f86326137b29b41
Mindy Jen <jencmhep@gmail.com>
2021-11-03 08:22:18 -0400

    update_211103

---
+++ HackerRank_C++/encapsulations_2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//add class definitions below this line
+
+class Words {
+  public:
+    Words(vector<string>& n){
+      list_of_words = n;
+    }
+  
+    void CoutStrings() {
+       vector<int> vowel_sizes;
+       vector<int> non_vowel_lengths;
+       int size;
+       int length;
+       for (auto a : list_of_words) {
+         size = CountVowels(a, a.length());
+         vowel_sizes.push_back(size);
+         length = CountNonVowels(a, a.length());
+         non_vowel_lengths.push_back(length);
+       }
+       /*
+       for (int i = 0; i < vowel_sizes.size(); i++) {
+         if (i == vowel_sizes.size()-1) {
+           cout << vowel_sizes.at(i) << endl;
+         }
+         else {
+           cout << vowel_sizes.at(i) << ',';
+         }
+       }
+      */
+      for (int i = 0; i < non_vowel_lengths.size(); i++) {
+         if (i == non_vowel_lengths.size()-1) {
+           cout << non_vowel_lengths.at(i) << endl;
+         }
+         else {
+           cout << non_vowel_lengths.at(i) << ',';
+         }
+       }
+     }
+  
+  private:
+    vector<string> list_of_words;
+  
+    bool IsVowel(char ch) {
+      ch = toupper(ch);
+      return (ch=='A' || ch=='E' || ch=='I' ||
+              ch=='O' || ch=='U');
+    }
+  
+    int CountVowels(string str, int n) {
+      if (n == 1) {
+        return IsVowel(str[n-1]);
+      }
+      return CountVowels(str, n-1) + IsVowel(str[n-1]);
+    }
+  
+    bool NotVowel(char ch) {
+      ch = toupper(ch);
+      return (ch!='A' && ch!='E' && ch!='I' &&
+              ch!='O' && ch!='U');
+    }
+
+    int CountNonVowels(string str, int n) {
+      if (n == 1) {
+        return NotVowel(str[n-1]);
+      }
+      return CountNonVowels(str, n-1) + NotVowel(str[n-1]);
+    }
+};
+
+//add class definitions above this line
+
+
+int main() {
+  
+  //add code below this line
+  
+    vector<string> list = {"house", "cake", "pancake"};
+    Words vowels(list);
+    vowels.CoutStrings();
+
+  //add code above this line
+  
+  return 0;
+  
+}
