d14c4dca045409c0f6b3df5e3a278e39c0e2bf28
Mindy Jen <jencmhep@gmail.com>
2021-11-12 15:09:55 -0500

    update_211112

---
+++ HackerRank_C++/AdvancedCpps_3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+//add class definitions below this line
+
+enum grades {A, B, C = 5, D};
+
+//add class definitions above this line
+
+int main() {
+  
+  //add code below this line
+
+  grades grade;
+
+  grade = D;
+  cout << "Grade = " << grade << endl;
+
+  //add code above this line
+  
+  return 0;
+  
+}
