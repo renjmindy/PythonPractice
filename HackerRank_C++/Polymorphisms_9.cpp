0070278faaf4a0463766e97132c7a4fc4a671d35
Mindy Jen <jencmhep@gmail.com>
2021-11-11 22:24:22 -0500

    update_211111

---
+++ HackerRank_C++/Polymorphisms_9.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//DO NOT EDIT/////////////////////////////////
+class Vehicle {                             //
+  public:                                   //
+    virtual void Distance(double time) = 0; //
+};                                          //
+//////////////////////////////////////////////
+
+//add class definitions below this line
+
+class Airplane : public Vehicle {
+  public:
+    Airplane(double s) {
+      speed = s;
+    }
+  
+    void Distance(double time) {
+      cout << speed * time << endl;
+    }
+  
+  private:
+    double speed;
+  
+};
+
+//add class definitions above this line
+
+int main() {
+  
+  //DO NOT EDIT code below this line
+  
+  Airplane a(550.0);
+  a.Distance(2);
+  a.Distance(3.15);
+  a.Distance(10.63);
+  Airplane a2(228.5);
+  a2.Distance(2);
+  a2.Distance(3.15);
+  a2.Distance(10.63);
+
+  //DO NOT EDIT code above this line
+  
+  return 0;
+  
+}
