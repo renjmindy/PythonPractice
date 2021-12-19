23519b9da39daf58fd06c65cdbc42b76d1b2683e
Mindy Jen <jencmhep@gmail.com>
2021-12-19 14:58:04 -0500

    update_211219

---
+++ LeetCode/insert_interval.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        if (intervals.size() <= 1) {
+            return intervals;
+        }
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int> > ans;
+        ans.push_back(intervals[0]);
+        for (int curr = 1, prev = 0; curr < intervals.size(); curr++) {
+            if (ans[prev][1] >= intervals[curr][0]) {
+                ans[prev][1] = max(ans[prev][1], intervals[curr][1]);
+            }
+            else {
+                ans.push_back(intervals[curr]);
+                prev++;
+            }
+        }
+        return ans;
+    }
+};
