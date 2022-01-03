99b349c19efe0d8cd7204d1bc81b7c20e4f05ffe
Mindy Jen <jencmhep@gmail.com>
2022-01-03 07:48:26 -0500

    update_220103

---
+++ HackerRank_DS/tree_level_order_traversal.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()) {
+            Node *n = q.front();
+            cout << n->data << " ";
+            q.pop();
+            if (n->left) {
+                q.push(n->left);
+            }
+            if (n->right) {
+                q.push(n->right);
+            }
+        }
+    }
+
+}; //End of Solution
