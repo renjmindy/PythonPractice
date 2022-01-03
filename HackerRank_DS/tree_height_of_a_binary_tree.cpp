2770de2000a388df891a2e464a07e7797c877686
Mindy Jen <jencmhep@gmail.com>
2022-01-02 19:31:34 -0500

    update_220102

---
+++ HackerRank_DS/tree_height_of_a_binary_tree.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        int l = -1, r = -1;
+        if (root->left) {
+            l = height(root->left);
+        }
+        if (root->right) {
+            r = height(root->right);
+        }
+        return max(l, r) + 1;
+    }
+
+}; //End of Solution
