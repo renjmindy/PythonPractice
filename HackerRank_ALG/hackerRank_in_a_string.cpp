33cbca84e64d5f70a62087a7ae579cee8bb279c3
Mindy Jen <jencmhep@gmail.com>
2022-01-06 20:34:01 -0500

    update_220106

---
+++ HackerRank_ALG/hackerRank_in_a_string.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'hackerrankInString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string hackerrankInString(string s) {
+    bool found = false;
+    int len = 0, n = s.size();
+    string t = "hackerrank", ans;
+    for (int i = 0; i < t.size(); i++) {
+        found = false;
+        while (len < n) {
+            if (t[i] == s[len]) {
+                found = true;
+                len += 1;
+                break;
+            }
+            len += 1;
+        }
+        if (found) {
+            ans += t[i];
+        }
+        else {
+            return "NO";
+        }
+    }
+    return ans == t ? "YES" : "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = hackerrankInString(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
