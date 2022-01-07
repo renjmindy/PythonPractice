6d71804e271bd173bde70d302bbb4417fd524d23
Mindy Jen <jencmhep@gmail.com>
2022-01-06 21:01:46 -0500

    update_220106

---
+++ HackerRank_ALG/pangrams.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string pangrams(string s) {
+    for (char i = 'a'; i <= 'z'; i++) {
+        size_t foundc = s.find(i);
+        size_t foundl = s.find(toupper(i));
+        if (foundc == string::npos && foundl == string::npos) {
+            return "not pangram";
+        }
+    }
+    return "pangram";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = pangrams(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
