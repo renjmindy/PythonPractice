0a4b2561e31bfdc67ea1567e646e3d8fb50a82b3
Mindy Jen <jencmhep@gmail.com>
2022-01-16 19:44:44 -0500

    update_220116

---
+++ HackerRank_ALG/fibonacci_modified.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'fibonacciModified' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER t1
+#  2. INTEGER t2
+#  3. INTEGER n
+#
+
+def fibonacciModified(t1, t2, n):
+    # Write your code here
+    dp = list()
+    dp.append(t1)
+    dp.append(t2)
+    for i in range(2, n, 1):
+        dp.append(dp[i - 1]**2 + dp[i - 2])
+    return dp[-1] 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    t1 = int(first_multiple_input[0])
+
+    t2 = int(first_multiple_input[1])
+
+    n = int(first_multiple_input[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
