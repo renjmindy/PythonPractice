1ac0e67fd3cdf00f696e836c07f10f9f81c0f07a
Mindy Jen <jencmhep@gmail.com>
2022-01-22 15:07:55 -0500

    update_220122

---
+++ Amazon_LeetCode_Python_C++/ArrayString/189_rotate_array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void reverse(vector<int>& nums, int left, int right) {
+        while (left < right) {
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+    
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1);
+    }
+};
