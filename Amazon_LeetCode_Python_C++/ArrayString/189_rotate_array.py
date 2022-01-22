02bd52440fe52f0e7a6cfe471bf3402e6b180ece
Mindy Jen <jencmhep@gmail.com>
2022-01-22 15:17:23 -0500

    update_220122

---
+++ Amazon_LeetCode_Python_C++/ArrayString/189_rotate_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, nums: List[int], left: int, right: int) -> None:
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+    
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, len(nums) - 1)
