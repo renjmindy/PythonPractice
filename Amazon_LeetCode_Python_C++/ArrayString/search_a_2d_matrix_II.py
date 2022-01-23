1df4c49010245f1df535ab97785c6234b49ef28d
Mindy Jen <jencmhep@gmail.com>
2022-01-22 21:34:46 -0500

    Delete search_a_2d_matrix_II.py

--- Amazon_LeetCode_Python_C++/ArrayString/search_a_2d_matrix_II.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            if target < matrix[i][0]:
-                break
-            else:
-                if target in matrix[i]:
-                    return True
-        return False
