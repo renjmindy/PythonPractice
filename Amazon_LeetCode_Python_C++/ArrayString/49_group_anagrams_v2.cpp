097578c9e0c25ac51bbe2f85a918bbb6ab8a643f
Mindy Jen <jencmhep@gmail.com>
2022-01-28 14:24:22 -0500

    update_220128

---
+++ Amazon_LeetCode_Python_C++/ArrayString/49_group_anagrams_v2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string> > ans;
+        unordered_map<string, vector<string> > mp;
+        for (int i = 0; i < strs.size(); i++) {
+            string str = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            mp[strs[i]].push_back(str);
+        }
+        for (auto &m : mp) {
+            ans.push_back(m.second);
+        }
+        return ans;
+    }
+}; 
