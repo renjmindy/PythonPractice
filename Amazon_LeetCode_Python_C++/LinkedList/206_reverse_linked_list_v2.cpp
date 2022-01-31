374103d7e0c78cf97cb80e6d08004fce2de6b176
Mindy Jen <jencmhep@gmail.com>
2022-01-31 10:02:17 -0500

    update_220131

---
+++ Amazon_LeetCode_Python_C++/LinkedList/206_reverse_linked_list_v2.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head, ListNode* prev=nullptr) {
+        if (!head) {
+            return prev;
+        }
+        //swap(next, head->next);
+        ListNode *next = head->next;
+        head->next = prev;
+        return reverseList(next, head);
+    }
+};
