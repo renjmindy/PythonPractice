0ccf8ce75ade2a86ca056510314c674d46ef2bce
Mindy Jen <jencmhep@gmail.com>
2022-02-05 17:39:36 -0500

    update_220205

---
+++ Amazon_LeetCode_Python_C++/TreesGraphs/590_n-ary_tree_postorder_traversal.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    
+private:
+    void postOrder(Node *root, vector<int> &ans) {
+        if (!root) {
+            return;
+        }
+        for (int i = 0; i < root->children.size(); i++) {
+            postOrder(root->children[i], ans);
+        }
+        ans.push_back(root->val);
+    }
+    
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        if (!root) {
+            return ans;
+        }
+        postOrder(root, ans);
+        return ans;
+    }
+};
