614730ca1c87489b01624f4a93eb1070f312a189
Mindy Jen <jencmhep@gmail.com>
2022-02-07 12:40:59 -0500

    update_220207

--- AWS_MLOps/project1/application.py
+++ AWS_MLOps/project1/application.py
@@ -1,14 +1,21 @@
 from flask import Flask, request
-app = Flask(__name__)
+import joblib
 
-@app.route('/')
+application = Flask(__name__)
+
+vectorizer = joblib.load("vectorizer.pkl")
+spamorham = joblib.load("spam_ham_model.pkl")
+
+@application.route('/')
 def hello_world():
     return "Hello World"
 
-@app.route('/spamorham', methods=['GET', 'POST'])
+@application.route('/spamorham', methods=['GET', 'POST'])
 def spamorham():
     message = request.args.get("message")
-    return message
+    vect_message = vectorizer.transform([message])
+    result = spamorham.predict(vect_message)[0]
+    return result
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
