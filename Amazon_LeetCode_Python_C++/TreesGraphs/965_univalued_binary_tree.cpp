9f7b424ee0b67a020d6d687af1fea2aca5fe70a0
Mindy Jen <jencmhep@gmail.com>
2022-02-09 12:41:31 -0500

    update_220209

---
+++ Amazon_LeetCode_Python_C++/TreesGraphs/965_univalued_binary_tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+private:
+    vector<int> ans;
+    void preOrder(TreeNode *root, vector<int> &ans) {
+        if (!root) {
+            return;
+        }
+        ans.push_back(root->val);
+        preOrder(root->left, ans);
+        preOrder(root->right, ans);
+    }
+    
+public:
+    bool isUnivalTree(TreeNode* root) {
+        preOrder(root, ans);
+        for (auto &a : ans) {
+            if (a != ans[0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
