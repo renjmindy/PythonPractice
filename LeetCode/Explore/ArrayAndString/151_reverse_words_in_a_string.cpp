cc7fea44d4eee33ec7173b89436c8d0206b3d703
Mindy Jen <jencmhep@gmail.com>
2022-02-12 14:07:12 -0500

    update_220212

---
+++ LeetCode/Explore/ArrayAndString/151_reverse_words_in_a_string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+      reverse(s.begin(), s.end());
+      int r = 0, l = 0;
+      string ans;
+      while (l < s.size()) {
+        while (r < s.size() && !isblank(s[r])) {
+          r++;
+        }
+        if (r > l) {
+          reverse(s.begin() + l, s.begin() + r);
+          for (int i = l; i < r; i++) {
+            ans += s[i];
+          }
+          ans += ' ';
+        }
+        r++;
+        l = r;
+      }
+      ans.erase(remove(ans.end() - 1, ans.end(), ' '), ans.end());
+      return ans;
+    }
+};
