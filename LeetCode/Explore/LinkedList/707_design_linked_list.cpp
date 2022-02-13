d5b864a98febbecd573620ec88b290a6b686042f
Mindy Jen <jencmhep@gmail.com>
2022-02-13 14:10:54 -0500

    update_220213

---
+++ LeetCode/Explore/LinkedList/707_design_linked_list.cpp
@@ -0,0 +1,49 @@
+class MyLinkedList {
+  
+private:
+  vector<int> vp;
+  
+public:
+  MyLinkedList() {}
+  
+  int get(int index) {
+    if (index < 0 || index >= vp.size()) {
+      return -1;
+    }
+    else {
+      return vp[index];
+    }
+  }
+  
+  void addAtHead(int val) {
+    vp.insert(vp.begin(), val);
+  }
+  
+  void addAtTail(int val) {
+    vp.push_back(val);
+  }
+    
+  void addAtIndex(int index, int val) {
+    if (index < 0 || index > vp.size()) {
+      return;
+    }
+    vp.insert(vp.begin() + index, val);
+  }
+    
+  void deleteAtIndex(int index) {
+    if (index < 0 || index >= vp.size()) {
+      return;
+    }
+    vp.erase(vp.begin() + index);
+  }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList* obj = new MyLinkedList();
+ * int param_1 = obj->get(index);
+ * obj->addAtHead(val);
+ * obj->addAtTail(val);
+ * obj->addAtIndex(index,val);
+ * obj->deleteAtIndex(index);
+ */
