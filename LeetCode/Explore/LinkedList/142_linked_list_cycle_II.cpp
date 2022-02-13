e5e78e110a1bd3584bcd553fe43db1d91d435946
Mindy Jen <jencmhep@gmail.com>
2022-02-13 15:48:51 -0500

    update_220213

---
+++ LeetCode/Explore/LinkedList/142_linked_list_cycle_II.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+      ListNode *f = head, *s = head;
+      if (!(f && f->next)) {
+        return NULL;
+      }
+      while (f && f->next) {
+        f = f->next->next;
+        s = s->next;
+        if (f == s) {
+          break;
+        }
+      }
+      if (!(f && f->next)) {
+        return NULL;
+      }
+      while (s != head) {
+        s = s->next;
+        head = head->next;
+      }
+      return head;
+    }
+};
