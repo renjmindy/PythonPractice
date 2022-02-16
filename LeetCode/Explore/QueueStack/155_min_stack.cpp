42e4d2c9d20bf33c645977df0ab2a4653bbfa6a8
Mindy Jen <jencmhep@gmail.com>
2022-02-15 20:09:18 -0500

    update_220215

---
+++ LeetCode/Explore/QueueStack/155_min_stack.cpp
@@ -0,0 +1,34 @@
+class MinStack {
+  
+private:
+  vector<int> vp;
+  
+public:
+  MinStack() {}
+    
+  void push(int val) {
+    vp.push_back(val);
+  }
+    
+  void pop() {
+    //vp.pop_back();
+    vp.erase(vp.end() - 1);
+  }
+    
+  int top() {
+    return vp.back();
+  }
+    
+  int getMin() {
+    return *min_element(vp.begin(), vp.end());
+  }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
