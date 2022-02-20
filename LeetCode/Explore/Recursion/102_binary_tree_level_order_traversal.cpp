e6a4ab6c7efbdf8c93f7731daa5662fe4b2338c9
Mindy Jen <jencmhep@gmail.com>
2022-02-19 19:56:06 -0500

    update_220219

---
+++ LeetCode/Explore/Recursion/102_binary_tree_level_order_traversal.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  
+private:
+  vector<vector<int> > ans;
+  void preOrder(TreeNode *root, int level, vector<vector<int> > &ans) {
+    if (!root) {
+      return;
+    }
+    if (level == ans.size()) {
+      ans.push_back({root->val});
+    }
+    else {
+      ans[level].push_back(root->val);
+    }   
+    preOrder(root->left, level + 1, ans);
+    preOrder(root->right, level + 1, ans);
+  }
+  
+public:
+  vector<vector<int>> levelOrder(TreeNode* root) {
+    preOrder(root, 0, ans);
+    return ans;
+  }
+};
