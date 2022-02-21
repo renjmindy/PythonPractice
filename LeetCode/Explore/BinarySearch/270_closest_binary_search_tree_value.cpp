0d55ec46f02c2f6e48ba3cc7b3b298727bfa3d0b
Mindy Jen <jencmhep@gmail.com>
2022-02-21 09:46:26 -0500

    update_220221

---
+++ LeetCode/Explore/BinarySearch/270_closest_binary_search_tree_value.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+private:
+  vector<int> nums;
+  void inOrder(TreeNode *root, vector<int> &nums) {
+    if (!root) {
+      return;
+    }
+    inOrder(root->left, nums);
+    nums.push_back(root->val);
+    inOrder(root->right, nums);
+  }
+    
+public:
+  int closestValue(TreeNode* root, double target) {
+    inOrder(root, nums);
+    int minLeaf = 0; 
+    double minVal = DBL_MAX;
+    for (int i = 0; i < nums.size(); i++) {
+      if (minVal > abs(nums[i] - target)) {
+        minVal = abs(nums[i] - target);
+        minLeaf = nums[i];
+      }
+    }
+    return minLeaf;
+  }
+};
