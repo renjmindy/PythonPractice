ead14a4ba9121f44f7f3e26f5bd09d3b27b9b63f
Mindy Jen <jencmhep@gmail.com>
2022-02-21 11:52:09 -0500

    update_220221

---
+++ LeetCode/Explore/BinarySearch/153_find_minimum in_rotated_sorted_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  int findMin(vector<int>& nums) {
+    int pivot, l = 0, r = nums.size() - 1;
+    while (l < r) {
+      pivot = l + (r - l) / 2;
+      if (nums[pivot] > nums[r]) {
+        l = pivot + 1;
+      }
+      else {
+        r = pivot;
+      }
+    }
+    return nums[l];
+  }
+};
