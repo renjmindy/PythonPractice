b83f7bf34cd5844222105fb37687e2869ecb608e
Mindy Jen <jencmhep@gmail.com>
2022-02-21 16:30:09 -0500

    update_220221

---
+++ LeetCode/Explore/BinarySearch/287_find_the_duplicate_number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+      unordered_map<int, int> mp;
+      for (int i = 0; i < nums.size(); i++) {
+        if (mp.count(nums[i])) {
+          return nums[i];
+        }
+        mp[nums[i]]++;
+      }
+      return -1;
+    }
+};
