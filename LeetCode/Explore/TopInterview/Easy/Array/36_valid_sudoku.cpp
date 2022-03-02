b99b69003dec16e17efc528e3cdbbdafc0c82ce3
Mindy Jen <jencmhep@gmail.com>
2022-03-02 08:16:25 -0500

    update_220302

---
+++ LeetCode/Explore/TopInterview/Easy/Array/36_valid_sudoku.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  bool isValidSudoku(vector<vector<char>>& board) {
+    unordered_set<string> sp;
+    for (int i = 0; i < board.size(); i++) {
+      for (int j = 0; j < board[0].size(); j++) {
+        if (board[i][j] != '.') {
+          int num = board[i][j] - '0';
+          string s1 = "row" + to_string(i) + to_string(num);
+          string s2 = "col" + to_string(j) + to_string(num);
+          string s3 = "box" + to_string(i/3 * 3 + j/3) + to_string(num);
+          if (sp.count(s1) || sp.count(s2) || sp.count(s3)) {
+            return false;
+          }
+          sp.insert(s1);
+          sp.insert(s2);
+          sp.insert(s3);
+        }
+      }
+    }
+    return true;
+  }
+};
