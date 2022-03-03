4677346c89dc3d979343305ad0288b5a60d91798
Mindy Jen <jencmhep@gmail.com>
2022-03-03 11:44:54 -0500

    update_220303

---
+++ LeetCode/Explore/TopInterview/Medium/SortingAndSearching/75_sort_colors_v2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  void sortColors(vector<int>& nums) {
+    int l = 0, cur = 0, r = nums.size() - 1;
+    while (cur <= r) {
+      if (!nums[cur]) {
+        swap(nums[cur], nums[l]);
+        cur++;
+        l++;
+      }
+      else if (nums[cur] == 2) {
+        swap(nums[cur], nums[r]);
+        r--;
+      }
+      else {
+        cur++;
+      }
+    }
+    /*for (int r = 0, l = 0; r < nums.size(); r++) {
+      if (nums[r]) {
+        swap(nums[r], nums[l]);
+        l++;
+      }
+    }*/
+    //reverse(nums.begin(), nums.end());
+  }
+};
