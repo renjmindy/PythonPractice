ea1304b7cc390ed389dd9e34d4a873d6dd97132e
Mindy Jen <jencmhep@gmail.com>
2022-03-04 11:48:43 -0500

    update_220304

---
+++ LeetCode/Explore/TopInterview/Medium/Trees/103_binary_tree_zigzag_level_order_traversal.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  
+private:
+  vector<int> ans;
+  
+  int findMax(TreeNode *root) {
+    if (!root) {
+      return 0;
+    }
+    return 1 + max(findMax(root->left), findMax(root->right));
+  }
+  
+  void preOrder(TreeNode *root, int level, vector<int> &ans) {
+    if (!root) {
+      return;
+    }
+    if (level == 0) {
+      ans.push_back(root->val);
+      return;
+    }
+    preOrder(root->left, level - 1, ans);
+    preOrder(root->right, level - 1, ans);
+  }
+  
+  
+public:
+  vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+    vector<vector<int> > res;
+    for (int i = 0; i < findMax(root); i++) {
+      vector<int> ans;
+      preOrder(root, i, ans);
+      if (i % 2) {
+        reverse(ans.begin(), ans.end());
+      }
+      res.push_back(ans);
+    }
+    return res;
+  }
+};
