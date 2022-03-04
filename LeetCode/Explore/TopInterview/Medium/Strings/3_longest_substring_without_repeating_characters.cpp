5061387a5714813100094774f46dc5586cab4b2e
Mindy Jen <jencmhep@gmail.com>
2022-03-03 23:41:24 -0500

    update_220303

---
+++ LeetCode/Explore/TopInterview/Medium/Strings/3_longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+      int maxLen = 0;
+      unordered_map<char, int> mp;
+      for (int r = 0, l = 0; r < s.size(); r++) {
+        if (mp.count(s[r])) {
+          l = max(l, mp[s[r]]);
+        }
+        maxLen = max(maxLen, r - l + 1);
+        mp[s[r]] = r + 1;
+      }
+      return maxLen;
+    }
+};
