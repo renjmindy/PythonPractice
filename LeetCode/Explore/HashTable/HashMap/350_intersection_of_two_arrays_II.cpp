eb6c35d81f04e279f1703f40a68099210be352df
Mindy Jen <jencmhep@gmail.com>
2022-03-08 21:00:58 -0500

    update_220308

---
+++ LeetCode/Explore/HashTable/HashMap/350_intersection_of_two_arrays_II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    unordered_map<int, int> mp;
+    vector<int> ans;
+    for (int i = 0; i < nums1.size(); i++) {
+      mp[nums1[i]]++;
+    }
+    for (int i = 0; i < nums2.size(); i++) {
+      if (mp.count(nums2[i])) {
+        if (mp[nums2[i]] > 1) {
+          mp[nums2[i]]--;
+        }
+        else {
+          mp.erase(nums2[i]);
+        }
+        ans.push_back(nums2[i]);
+      }
+    }
+    return ans;
+  }
+};
