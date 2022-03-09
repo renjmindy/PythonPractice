f9c5b3facd5119367a0e46c5511239a8fadca6df
Mindy Jen <jencmhep@gmail.com>
2022-03-09 05:18:25 -0500

    update_220309

---
+++ LeetCode/Explore/HashTable/DesignKey/49_group_anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string> > mp;
+    vector<vector<string> > ans;
+    for (auto &str : strs) {
+      string tmp = str;
+      sort(tmp.begin(), tmp.end());
+      mp[tmp].push_back(str);
+    }
+    for (auto &m : mp) {
+      ans.push_back(m.second);
+    }
+    return ans;
+  }
+};
