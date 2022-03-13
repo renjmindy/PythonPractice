2ff787934345669518ac8172ca107d0e3cacc14e
Mindy Jen <jencmhep@gmail.com>
2022-03-13 18:07:57 -0400

    update_220313

---
+++ LeetCode/Explore/TopInterview/Blind75/Array/167_two_sum_II_input_array_is_sorted_2pointers.cpp
@@ -0,0 +1,24 @@
+// two pointers
+// Complexity Analysis
+// Time complexity: O(n)O(n). The input array is traversed at most once. Thus the time complexity is O(n)O(n).
+// Space complexity: O(1)O(1). We only use additional space to store two indices and the sum, so the space complexity is O(1)O(1).
+
+class Solution {
+public:
+  vector<int> twoSum(vector<int>& numbers, int target) {
+    int pivot, l = 0, r = numbers.size() - 1;
+    while (l < r) {
+      int sum = numbers[l] + numbers[r];
+      if (sum < target) {
+        l++;
+      }
+      else if (sum > target) {
+        r--;
+      }
+      else {
+        return {l + 1, r + 1};
+      }
+    }
+    return {-1, -1};
+  }
+};
