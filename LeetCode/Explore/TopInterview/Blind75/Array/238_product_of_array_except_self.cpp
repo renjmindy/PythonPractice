49c773358033531ae7925f48720500e4ddfbc4cb
Mindy Jen <jencmhep@gmail.com>
2022-03-13 09:10:40 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/238_product_of_array_except_self.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/238_product_of_array_except_self.cpp
@@ -1,1 +1,24 @@
-// ...
+// two pass hash table
+// Complexity analysis
+// Time complexity : O(N)O(N) where NN represents the number of elements in the input array. 
+// we use one iteration to construct the array LL, one to update the array answeranswer.
+// Space complexity : O(1)O(1) since don't use any additional array for our computations.
+// The problem statement mentions that using the answeranswer array doesn't add to the space complexity.
+
+class Solution {
+public:
+  vector<int> productExceptSelf(vector<int>& nums) {
+    int mul = 1;
+    vector<int> dp(nums.size(), 1);
+    for (int i = 0; i < nums.size(); i++) {
+      dp[i] *= mul;
+      mul *= nums[i];
+    }
+    mul = 1;
+    for (int i = nums.size() - 1; i >= 0; i--) {
+      dp[i] *= mul;
+      mul *= nums[i];
+    }
+    return dp;
+  }
+};
