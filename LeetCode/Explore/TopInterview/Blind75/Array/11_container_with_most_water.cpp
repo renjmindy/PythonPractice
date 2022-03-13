6bde675db31dac4958853190d925131172d5fc0c
Mindy Jen <jencmhep@gmail.com>
2022-03-13 15:31:16 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/11_container_with_most_water.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/11_container_with_most_water.cpp
@@ -1,1 +1,21 @@
-// ...
+// two pass binary search maximal area
+// Complexity Analysis
+// Time complexity : O(n)O(n). Single pass.
+// Space complexity : O(1)O(1). Constant space is used.
+
+class Solution {
+public:
+  int maxArea(vector<int>& height) {
+    int maxArea = INT_MIN, l = 0, r = height.size() - 1;
+    while (l < r) {
+      maxArea = max(maxArea, min(height[l], height[r]) * (r - l));
+      if (height[l] < height[r]) {
+        l++;
+      }
+      else {
+        r--;
+      }
+    }
+    return maxArea;
+  }
+};
