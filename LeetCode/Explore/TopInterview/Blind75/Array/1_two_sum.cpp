9d3371976f7113d1a642194cdc3aedea085a60fa
Mindy Jen <jencmhep@gmail.com>
2022-03-13 06:44:42 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/1_two_sum.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/1_two_sum.cpp
@@ -1,1 +1,16 @@
-// ... 
+// hash table 
+class Solution {
+public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++) {
+      if (mp.count(target - nums[i])) {
+        ans.push_back(mp[target - nums[i]]);
+        ans.push_back(i);
+      }
+      mp[nums[i]] = i;
+    }
+    return ans;
+  }
+};
