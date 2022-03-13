71cdeeb480d4ca09b8a6b051e172bd442bca0b4d
Mindy Jen <jencmhep@gmail.com>
2022-03-13 12:32:23 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/33_search in_rotated_sorted_array.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/33_search in_rotated_sorted_array.cpp
@@ -1,1 +1,34 @@
-// ...
+// one pass binary search
+// Complexity Analysis
+// Time complexity: \mathcal{O}(\log{N})O(logN).
+// Space complexity: \mathcal{O}(1)O(1).
+
+class Solution {
+public:
+  int search(vector<int>& nums, int target) {
+    int pivot, l = 0, r = nums.size() - 1;
+    while (l <= r) {
+      pivot = l + (r - l) / 2;
+      if (nums[pivot] == target) {
+        return pivot;
+      }
+      else if (nums[l] <= nums[pivot]) {
+        if (target >= nums[l] && target < nums[pivot]) {
+          r = pivot - 1;
+        }
+        else {
+          l = pivot + 1;
+        }
+      }
+      else {
+        if (target > nums[pivot] && target <= nums[r]) {
+          l = pivot + 1;
+        }
+        else {
+          r = pivot - 1;
+        }
+      }
+    }
+    return -1;
+  }
+};
