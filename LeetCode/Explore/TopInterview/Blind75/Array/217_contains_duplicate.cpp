7ca63ff088e959065ec188528dc13027c1d64f4b
Mindy Jen <jencmhep@gmail.com>
2022-03-13 08:03:45 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/217_contains_duplicate.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/217_contains_duplicate.cpp
@@ -1,1 +1,23 @@
-// ...
+// one pass hash table
+// Complexity Analysis
+// Time complexity: O(n)O(n). We do search() and insert() for nn times and each operation takes constant time.
+// Space complexity: O(n)O(n). The space used by a hash table is linear with the number of elements in it.
+  
+class Solution {
+public:
+  bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> sp;
+    for (auto &num : nums) {
+      if (sp.count(num)) {
+        return true;
+      }
+      else {
+        sp.insert(num);
+      }
+    }
+    return false;
+  }
+};
+
+[Problem 219 Contains Duplicate II](https://leetcode.com/problems/contains-duplicate-ii/)
+[Problem 220 Contains Duplicate III](https://leetcode.com/problems/contains-duplicate-iii/)
