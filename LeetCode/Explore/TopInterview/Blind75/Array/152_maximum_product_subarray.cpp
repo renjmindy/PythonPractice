439d3c74a2074342c09672900689cf074c8d6f52
Mindy Jen <jencmhep@gmail.com>
2022-03-13 10:11:40 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/152_maximum_product_subarray.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/152_maximum_product_subarray.cpp
@@ -1,4 +1,11 @@
-// one pass dynamical programming, 
+// one pass dynamical programming, Kadane's Algorithm
+// Complexity Analysis
+// Time complexity : O(N)O(N) where NN is the size of nums. 
+// The algorithm achieves linear runtime since we are going through nums only once.
+// Space complexity : O(1)O(1) since no additional space is consumed rather than 
+// variables which keep track of the maximum product so far, the minimum product so far, 
+// current variable, temp variable, and placeholder variable for the result.
+
 class Solution {
 public:
   int maxProduct(vector<int>& nums) {
