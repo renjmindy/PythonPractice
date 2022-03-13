3882d74d6f203ee27fab1ae3990099e0bfd5c9ed
Mindy Jen <jencmhep@gmail.com>
2022-03-13 06:52:36 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/1_two_sum.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/1_two_sum.cpp
@@ -1,4 +1,8 @@
-// hash table 
+// one pass hash table 
+// Complexity Analysis
+// Time complexity: O(n)O(n). We traverse the list containing nn elements only once. Each lookup in the table costs only O(1)O(1) time.
+// Space complexity: O(n)O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements.
+  
 class Solution {
 public:
   vector<int> twoSum(vector<int>& nums, int target) {
