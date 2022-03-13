90b58abc3f080c893edf89b7d5831be95eb7ea58
Mindy Jen <jencmhep@gmail.com>
2022-03-12 19:17:53 -0500

    update_220312

---
+++ LeetCode/Explore/DynamicProgramming/1155_number_of_dice_rolls_with_target_sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  int numRollsToTarget(int n, int k, int target) {
+    int mod = 1e9 + 7;
+    vector<vector<int>> dp(30001 , vector<int>(n + 1));
+    
+    dp[target][0] = 1;
+	  for(int count = 1 ; count <= n  ; count++) {
+		  for(int sum = target ; sum >= 0 ; sum--) {
+			  int ways = 0;
+			  for(int face = 1 ; face <= k ; face++) {
+				  ways = (ways + dp[sum + face][count - 1]) % mod;
+			  }
+			  dp[sum][count] = ways;
+		  }
+	  }
+	  return dp[0][n];     
+  }
+};
