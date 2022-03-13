1976763b1bb036e182bc456cea599643ac2bc57e
Mindy Jen <jencmhep@gmail.com>
2022-03-13 10:09:39 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/152_maximum_product_subarray.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/152_maximum_product_subarray.cpp
@@ -1,1 +1,14 @@
-// ...
+// one pass dynamical programming, 
+class Solution {
+public:
+  int maxProduct(vector<int>& nums) {
+    int maxProd = nums[0], curMax = nums[0], curMin = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+      int tmpMax = max(nums[i], max(curMax * nums[i], curMin * nums[i]));
+      curMin = min(nums[i], min(curMax * nums[i], curMin * nums[i]));
+      curMax = tmpMax;
+      maxProd = max(maxProd, curMax);
+    }
+    return maxProd;
+  }
+};
