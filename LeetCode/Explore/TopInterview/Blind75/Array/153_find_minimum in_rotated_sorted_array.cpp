79737e4b90244d7a9a93355c81fd6c0c8f4d1931
Mindy Jen <jencmhep@gmail.com>
2022-03-13 11:27:08 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/153_find_minimum in_rotated_sorted_array.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/153_find_minimum in_rotated_sorted_array.cpp
@@ -1,1 +1,21 @@
-// ...
+// one pass binary search
+// Complexity Analysis
+// Time Complexity : Same as Binary Search O(\log N)O(logN)
+// Space Complexity : O(1)O(1)
+
+class Solution {
+public:
+  int findMin(vector<int>& nums) {
+    int pivot, l = 0, r = nums.size() - 1;
+    while (l < r) {
+      pivot = l + (r - l) / 2;
+      if (nums[pivot] > nums[r]) {
+        l = pivot + 1;
+      }
+      else {
+        r = pivot;
+      }
+    }
+    return nums[l];
+  }
+};
