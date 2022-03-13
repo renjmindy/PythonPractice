cbb073bf37c5c2049e3b5ddc9be9c2e72354eee0
Mindy Jen <jencmhep@gmail.com>
2022-03-13 06:50:34 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/Array/121_best_time_to_buy_and_sell_stock.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Array/121_best_time_to_buy_and_sell_stock.cpp
@@ -1,1 +1,20 @@
-// ...
+// one pass
+// Complexity Analysis
+// Time complexity: O(n)O(n). Only a single pass is needed.
+// Space complexity: O(1)O(1). Only two variables are used.
+  
+class Solution {
+public:
+  int maxProfit(vector<int>& prices) {
+    int maxProfit = 0, minPurchase = prices[0];
+    for (int i = 0; i < prices.size(); i++) {
+      if (prices[i] < minPurchase) {
+        minPurchase = prices[i];
+      }
+      else {
+        maxProfit = max(maxProfit, prices[i] - minPurchase);
+      }
+    }
+    return maxProfit;
+  }
+};
