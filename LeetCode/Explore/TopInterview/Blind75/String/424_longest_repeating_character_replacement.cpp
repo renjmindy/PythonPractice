2f32f158b1d4f24160def61b1e5b0ce4cfeb41bb
Mindy Jen <jencmhep@gmail.com>
2022-03-13 21:42:52 -0400

    update_220313

--- LeetCode/Explore/TopInterview/Blind75/String/424_longest_repeating_character_replacement.cpp
+++ LeetCode/Explore/TopInterview/Blind75/String/424_longest_repeating_character_replacement.cpp
@@ -1,1 +1,19 @@
-// ...
+// two pointers sliding window 
+
+class Solution {
+public:
+  int characterReplacement(string s, int k) {
+    unordered_map<char, int> mp;
+    int count = 0, ans = 0;
+    for (int r = 0, l = 0; r < s.size(); r++) {
+      mp[s[r]]++;
+      count = max(count, mp[s[r]]);
+      if (r -l + 1 - count > k) {
+        mp[s[l]]--;
+        l++;
+      }
+      ans = max(ans, r - l + 1);
+    }
+    return ans;
+  }
+};
