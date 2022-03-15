6471e06934d096ec32b8a48f10baf3d24572e0df
Mindy Jen <jencmhep@gmail.com>
2022-03-15 19:32:11 -0400

    update_220315

--- LeetCode/Explore/TopInterview/Blind75/DynamicProgramming/139_word_break.cpp
+++ LeetCode/Explore/TopInterview/Blind75/DynamicProgramming/139_word_break.cpp
@@ -1,1 +1,25 @@
-// ...
+// dynamical programming: bottom-up tabulation
+
+// Complexity Analysis
+// nn is the length of the input string.
+// Time complexity : O(n^3)O(n3). There are two nested loops, and substring computation at each iteration. 
+// Overall that results in O(n^3)O(n3) time complexity.
+// Space complexity : O(n)O(n). Length of pp array is n+1n+1.
+
+class Solution {
+public:
+  bool wordBreak(string s, vector<string>& wordDict) {
+    set<string> sp(wordDict.begin(), wordDict.end());
+    vector<bool> dp(s.size() + 1, false);
+    dp[0] = true;
+    for (int i = 1; i <= s.size(); i++) {
+      for (int j = 0; j < i; j++) {
+        if (dp[j] && sp.count(s.substr(j, i - j))) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+    return dp[s.size()];
+  }
+};
