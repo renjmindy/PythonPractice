289503d6552b6fc36481937b67a21425a12ee16b
Mindy Jen <jencmhep@gmail.com>
2022-03-16 12:58:47 -0400

    update_220316

--- LeetCode/Explore/TopInterview/Blind75/LinkedList/206_reverse_linked_list.cpp
+++ LeetCode/Explore/TopInterview/Blind75/LinkedList/206_reverse_linked_list.cpp
@@ -1,1 +1,25 @@
-// ...
+// Complexity analysis
+// Time complexity : O(n)O(n). Assume that nn is the list's length, the time complexity is O(n)O(n).
+// Space complexity : O(n)O(n). The extra space comes from implicit stack space due to recursion. The recursion could go up to nn levels deep.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* reverseList(ListNode* head, ListNode *pre = nullptr) {
+    if (!head) { // base case
+      return pre;
+    }
+    ListNode *next = head->next;
+    head->next = pre;
+    return reverseList(next, head);
+  }
+};
