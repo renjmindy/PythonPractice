e1b1556ac0f324e5ca1a0d7b5114bde94fa363ed
Mindy Jen <jencmhep@gmail.com>
2022-03-17 12:37:12 -0400

    update_220317

--- LeetCode/Explore/TopInterview/Blind75/Tree/112_path_sum.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Tree/112_path_sum.cpp
@@ -1,1 +1,33 @@
-// ...
+// Recursion
+
+// Complexity Analysis
+// Time complexity : we visit each node exactly once, thus the time complexity is \mathcal{O}(N)O(N), where NN is the number of nodes.
+// Space complexity : in the worst case, the tree is completely unbalanced, e.g. each node has only one child node, the recursion call would occur NN times 
+// (the height of the tree), therefore the storage to keep the call stack would be \mathcal{O}(N)O(N). But in the best case (the tree is completely balanced), 
+// the height of the tree would be \log(N)log(N). Therefore, the space complexity in this case would be \mathcal{O}(\log(N))O(log(N)).
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  
+public:
+  bool hasPathSum(TreeNode* root, int targetSum) {
+    if (!root) {
+      return false;
+    }
+    targetSum -= root->val;
+    if (!root->left && !root->right) {
+      return targetSum == 0;
+    }
+    return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+  }
+};
