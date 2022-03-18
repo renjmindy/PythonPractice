7690996cfafcdc77f93e9a6043ce544bcd923791
Mindy Jen <jencmhep@gmail.com>
2022-03-18 11:11:40 -0400

    update_220318

--- LeetCode/Explore/TopInterview/Blind75/Tree/98_validate_binary_search_tree.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Tree/98_validate_binary_search_tree.cpp
@@ -1,1 +1,29 @@
-// ...
+// Recursive Traversal with Valid Range
+
+// Complexity Analysis
+// Time complexity : \mathcal{O}(N)O(N) since we visit each node exactly once.
+// Space complexity : \mathcal{O}(N)O(N) since we keep up to the entire tree.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  bool isValidBST(TreeNode* root, long Min = LONG_MIN, long Max = LONG_MAX) {
+    if (!root) { // base case
+      return true;
+    }
+    if (root->val <= Min || root->val >= Max) {
+      return false;
+    }
+    return isValidBST(root->left, Min, root->val) && isValidBST(root->right, root->val, Max);
+  }
+};
