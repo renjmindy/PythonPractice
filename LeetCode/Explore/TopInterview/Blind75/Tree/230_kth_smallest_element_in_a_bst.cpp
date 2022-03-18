cbc72f1e7cbeb8b74179783a9454760ea1338034
Mindy Jen <jencmhep@gmail.com>
2022-03-18 11:47:44 -0400

    update_220318

--- LeetCode/Explore/TopInterview/Blind75/Tree/230_kth_smallest_element_in_a_bst.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Tree/230_kth_smallest_element_in_a_bst.cpp
@@ -1,1 +1,36 @@
-// ...
+// Recursive Inorder Traversal
+
+// Complexity Analysis
+// Time complexity : O(N)O(N) to build a traversal.
+// Space complexity : O(N)O(N) to keep an inorder traversal.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  
+private:
+  vector<int> vp;
+  void inOrder(TreeNode *root) {
+     if (!root) {
+      return;
+    }
+    inOrder(root->left);
+    vp.push_back(root->val);
+    inOrder(root->right);
+  }
+  
+public:
+  int kthSmallest(TreeNode* root, int k) {
+    inOrder(root);
+    return vp[k - 1];
+  }
+};
