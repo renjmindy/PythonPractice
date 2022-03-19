433f43a713482b37bf69f20bcb835272fc9c72ed
Mindy Jen <jencmhep@gmail.com>
2022-03-19 11:16:34 -0400

    update_220319

--- LeetCode/Explore/TopInterview/Blind75/Tree/211_design_add_and_search_words_data_structure.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Tree/211_design_add_and_search_words_data_structure.cpp
@@ -1,4 +1,16 @@
-// ...
+// trie : How to Implement Trie: addWord function
+// Complexity Analysis
+// Time complexity: \mathcal{O}(M)O(M), where MM is the key length. At each step, we either examine or create a node in the trie. That takes only MM operations.
+// Space complexity: \mathcal{O}(M)O(M). In the worst-case newly inserted key doesn't share a prefix with the keys already inserted in the trie. 
+// We have to add MM new nodes, which takes \mathcal{O}(M)O(M) space.
+
+// Search in Trie
+// Complexity Analysis
+// Time complexity: \mathcal{O}(M)O(M) for the "well-defined" words without dots, where MM is the key length, and NN is a number of keys, 
+// and \mathcal{O}(N \cdot 26 ^ M)O(N⋅26M) for the "undefined" words. That corresponds to the worst-case situation of searching an undefined 
+// word \underbrace{.........}_\text{M times} M times.........​​ which is one character longer than all inserted keys.
+// Space complexity: \mathcal{O}(1)O(1) for the search of "well-defined" words without dots, and up to \mathcal{O}(M)O(M) for the "undefined" words, 
+// to keep the recursion stack.
 
 class TrieNode{
 public:
