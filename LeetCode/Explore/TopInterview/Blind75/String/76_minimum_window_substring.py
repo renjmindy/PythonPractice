2aa1189d61c2e710e2f609092ad196bd9a731615
Mindy Jen <jencmhep@gmail.com>
2022-03-26 18:15:43 -0400

    update_220326

---
+++ LeetCode/Explore/TopInterview/Blind75/String/76_minimum_window_substring.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+      ans = ""
+      l = 0
+      counter = 0
+      mp = Counter(t)
+      
+      for r, c in enumerate(s):
+        if mp[c] > 0:
+          counter += 1
+        mp[c] -= 1
+        if counter < len(t):
+          continue
+          
+        while l < len(s) and mp[s[l]] < 0:
+          mp[s[l]] += 1
+          l += 1
+          
+        if len(ans) == 0 or r - l + 1 < len(ans):
+          ans = s[l:r + 1]
+          
+      return ans
