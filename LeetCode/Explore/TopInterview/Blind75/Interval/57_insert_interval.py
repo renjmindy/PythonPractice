247cab4e3ce9fd2f13662cf778454de802efb12d
Mindy Jen <jencmhep@gmail.com>
2022-03-28 08:51:54 -0400

    update_032822

---
+++ LeetCode/Explore/TopInterview/Blind75/Interval/57_insert_interval.py
@@ -0,0 +1,15 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+      intervals.append(newInterval)
+      ans = list()
+      intervals.sort()
+      ans.append(intervals[0])
+      
+      for i in range(1, len(intervals), 1):
+        if ans[-1][1] >= intervals[i][0]:
+          ans[-1][1] = max(ans[-1][1], intervals[i][1])
+        else:
+          ans.append(intervals[i])
+          
+      return ans
+      
