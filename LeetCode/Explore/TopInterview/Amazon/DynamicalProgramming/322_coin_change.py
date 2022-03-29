d756ecc6083b2b323d420b987f42f93236976a56
Mindy Jen <jencmhep@gmail.com>
2022-03-29 13:57:16 -0400

    update_220329

---
+++ LeetCode/Explore/TopInterview/Amazon/DynamicalProgramming/322_coin_change.py
@@ -0,0 +1,14 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [1000000] * (amount + 1)
+        dp[0] = 0
+        
+        for i in range(1, amount + 1, 1):
+          for coin in coins:
+            if coin <= i and dp[i - coin] < 1000000:
+              dp[i] = min(dp[i], dp[i - coin] + 1)
+              
+        if dp[amount] <= amount:
+          return dp[amount]
+        else:
+          return -1
