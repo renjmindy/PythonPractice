2710c5ee55c7a1fe6716960444fd8532562a8a01
Mindy Jen <jencmhep@gmail.com>
2022-03-29 22:04:47 -0400

    update_220329

---
+++ LeetCode/Explore/TopInterview/Amazon/SortingSearching/33_search_in_rotated_sorted_array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+      l, r = 0, len(nums) - 1
+      while l <= r:
+        pivot = l + (r - l) // 2
+        if nums[pivot] == target:
+          return pivot
+        if nums[l] <= nums[pivot]:
+          if target >= nums[l] and target < nums[pivot]:
+            r = pivot - 1
+          else:
+            l = pivot + 1
+        else:
+          if target > nums[pivot] and target <= nums[r]:
+            l = pivot + 1
+          else:
+            r = pivot - 1
+            
+      return -1
