f4c52f97610d4610ba09b75e0ee94e590ce10b03
Mindy Jen <jencmhep@gmail.com>
2022-03-29 22:37:03 -0400

    update_220329

---
+++ LeetCode/Explore/TopInterview/Amazon/SortingSearching/56_merge_intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = list()
+        intervals.sort()
+        ans.append(intervals[0])
+        
+        for i in range(1, len(intervals), 1):
+          if ans[-1][1] >= intervals[i][0]:
+            ans[-1][1] = max(ans[-1][1], intervals[i][1])
+          else:
+            ans.append(intervals[i])
+            
+        return ans
