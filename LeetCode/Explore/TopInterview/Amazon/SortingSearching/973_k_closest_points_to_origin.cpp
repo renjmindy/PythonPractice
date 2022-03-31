fa366fc262f9f55a9e615e01c6e5fbfc60d57827
Mindy Jen <jencmhep@gmail.com>
2022-03-30 22:29:42 -0400

    update_220330

---
+++ LeetCode/Explore/TopInterview/Amazon/SortingSearching/973_k_closest_points_to_origin.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+      typedef pair<int, int> P;
+      priority_queue<P, vector<P>, greater<P> > pq; // descending
+      vector<vector<int> > ans;
+      for (int i = 0; i < points.size(); i++) {
+        pq.push(make_pair(pow(points[i][0], 2) + pow(points[i][1], 2), i));
+      }
+      while (k) {
+        ans.push_back(points[pq.top().second]);
+        pq.pop();
+        k--;
+      }
+      return ans;
+    }
+};
