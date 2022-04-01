e5b8e448a4464eacc28f3683f7294954028c1c20
Mindy Jen <jencmhep@gmail.com>
2022-03-31 20:45:54 -0400

    update_220331

--- LeetCode/Explore/TopInterview/Amazon/SortingSearching/973_k_closest_points_to_origin.cpp
+++ LeetCode/Explore/TopInterview/Amazon/SortingSearching/973_k_closest_points_to_origin.cpp
@@ -2,26 +2,8 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
       typedef pair<int, int> P;
-      priority_queue<P, vector<P>, greater<P> > pq; // descending
+      priority_queue<P, vector<P>, greater<P> > pq; // ascending
       vector<vector<int> > ans;
-      for (int i = 0; i < points.size(); i++) {
-        pq.push(make_pair(pow(points[i][0], 2) + pow(points[i][1], 2), i));
-      }
-      while (k) {
-        ans.push_back(points[pq.top().second]);
-        pq.pop();
-        k--;
-      }
-      return ans;
-    }
-};
-
-class Solution {
-public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-      typedef pair<int, int> P;
-      priority_queue<P, vector<P>, greater<P> > pq;
-      vector<vector<int>> ans; 
       for (int i = 0; i < points.size(); i++) {
         pq.push(make_pair(pow(points[i][0], 2) + pow(points[i][1], 2), i));
       }
