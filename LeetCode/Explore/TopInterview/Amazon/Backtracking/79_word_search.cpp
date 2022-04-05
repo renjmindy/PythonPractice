e24ecf3304c42481fb97bab15ee5594f03448b0f
Mindy Jen <jencmhep@gmail.com>
2022-04-05 06:17:54 -0400

    update_220405

--- LeetCode/Explore/TopInterview/Amazon/Backtracking/79_word_search.cpp
+++ LeetCode/Explore/TopInterview/Amazon/Backtracking/79_word_search.cpp
@@ -2,13 +2,17 @@ class Solution {
   
 private:
   bool func(int i, int j, int idx, vector<vector<char> > &board, string &word, vector<int> &dx, vector<int> &dy) {
+    /* Step 1). check the bottom case. */
     if (idx == word.size()) {
       return true;
     }
+    /* Step 2). Check the boundaries. */
     if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[idx]) {
       return false;
     }
+    /* Step 3). explore the neighbors in DFS */
     char c = board[i][j];
+    // mark the path before the next exploration
     board[i][j] = '$';
     for (int k = 0; k < 4; k++) {
       int x = i + dx[k];
@@ -20,6 +24,7 @@ private:
         return true;
       }
     }
+    /* Step 4). clean up and return the result. */
     board[i][j] = c;
     return false;
   }
