512efbc57d7eba9e6753bfc5e10fe8f0586dfa80
Mindy Jen <jencmhep@gmail.com>
2022-04-08 12:06:23 -0400

    update_220408

---
+++ LeetCode/Explore/TopInterview/MicroSoft/ArrayString/48_rotate_image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  
+private:
+  void transpose(vector<vector<int> >& matrix) {
+    for (int i = 0; i < matrix.size(); i++) {
+      for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
+      }
+    }
+  }
+  
+  void reversal(vector<vector<int> >& matrix) {
+    for (int i = 0; i < matrix.size(); i++) {
+      reverse(matrix[i].begin(), matrix[i].end());
+    }
+  }
+  
+public:
+    void rotate(vector<vector<int>>& matrix) {
+      transpose(matrix);
+      reversal(matrix);
+    }
+};
