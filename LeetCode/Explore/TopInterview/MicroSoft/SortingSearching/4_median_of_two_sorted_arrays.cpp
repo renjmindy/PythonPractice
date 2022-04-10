4e874cf00e02e2c5e02fb1a7ff6f932503faeecf
Mindy Jen <jencmhep@gmail.com>
2022-04-10 16:13:51 -0400

    update_220410

---
+++ LeetCode/Explore/TopInterview/MicroSoft/SortingSearching/4_median_of_two_sorted_arrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+      for (int i = 0; i < nums2.size(); i++) {
+        nums1.push_back(nums2[i]);
+      }
+      sort(nums1.begin(), nums1.end());
+      if (nums1.size() & 1) {
+        return (double)nums1[nums1.size() / 2];
+      }
+      else {
+        return (double)((nums1[(nums1.size() / 2) - 1] + nums1[nums1.size() / 2]) * 1.0/ 2);
+      }
+    }
+};
