46591cd7be0abf85c10216f0c7fe28a005207abc
Mindy Jen <jencmhep@gmail.com>
2022-04-09 23:41:32 -0400

    update_220409

---
+++ LeetCode/Explore/TopInterview/MicroSoft/SortingSearching/26_remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+      sort(nums.begin(), nums.end());
+      int l = 0;
+      for (int r = 0; r < nums.size(); r++) {
+        if (nums[r] != nums[l]) {
+          l++;
+          nums[l] = nums[r];
+        }
+      }
+      return l + 1;
+    }
+};
