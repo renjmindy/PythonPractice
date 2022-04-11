00864c4a146ff110ce56091f1d879b0b0c4ffea7
Mindy Jen <jencmhep@gmail.com>
2022-04-11 01:20:02 -0400

    update_220411

--- LeetCode/Explore/TopInterview/Blind75/Graph/207_course_schedule.cpp
+++ LeetCode/Explore/TopInterview/Blind75/Graph/207_course_schedule.cpp
@@ -1,1 +1,38 @@
-// ...
+class Solution {
+  
+private:
+  bool dfs(int curr, vector<int> &vis1, vector<int> &vis2, vector<vector<int> > &graph) {
+    vis1[curr] = 1;
+    vis2[curr] = 1;
+    for (int i = 0; i < graph[curr].size(); i++) {
+      if (vis1[graph[curr][i]] == 0) {
+        if (dfs(graph[curr][i], vis1, vis2, graph)) {
+          return true;
+        }
+      }
+      else if (vis2[graph[curr][i]] == 1) {
+        return true;
+      }
+    }
+    vis2[curr] = 0;
+    return false;
+  }
+  
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+      vector<vector<int> > graph(numCourses);
+      vector<int> vis1(numCourses, 0);
+      vector<int> vis2(numCourses, 0);
+      for (int i = 0; i < prerequisites.size(); i++) {
+        graph[prerequisites[i][0]].push_back(prerequisites[i][1]);
+      }
+      for (int i = 0; i < numCourses; i++) {
+        if (vis1[i] == 0) {
+          if (dfs(i, vis1, vis2, graph)) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+};
