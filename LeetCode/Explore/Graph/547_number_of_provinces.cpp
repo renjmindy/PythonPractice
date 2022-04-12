24f3bf397607de42bbbd5013497022c04ec6f9fb
Mindy Jen <jencmhep@gmail.com>
2022-04-11 20:26:08 -0400

    update_220411

---
+++ LeetCode/Explore/Graph/547_number_of_provinces.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  
+private:
+  void dfs(int cur, vector<vector<int> > &isConnected, vector<bool> &visited) {
+    for (int i = 0; i < isConnected.size(); i++) {
+      if (isConnected[cur][i] && !visited[i]) {
+        visited[i] = true;
+        dfs(i, isConnected, visited);
+      }
+    }
+  }
+  
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+      vector<bool> visited(isConnected.size(), false);
+      int ans = 0;
+      for (int i = 0; i < isConnected.size(); i++) {
+        if (!visited[i]) {
+          dfs(i, isConnected, visited);
+          ans++;
+        }
+      }
+      return ans;
+    }
+};
