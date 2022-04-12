07e2e50e80a2f94630632c00123968d1aa67b1fb
Mindy Jen <jencmhep@gmail.com>
2022-04-11 21:09:52 -0400

    update_220411

--- LeetCode/Explore/Graph/323_number_of_connected_components_in_an_undirected_graph.cpp
+++ LeetCode/Explore/Graph/323_number_of_connected_components_in_an_undirected_graph.cpp
@@ -3,11 +3,11 @@ public:
     //convert the graph into adjacency list first 
     //number of dfs is the number of components
     
-    void dfs(vector<vector<int>>& adj, int node, vector<bool>& visited){
+    void dfs(vector<vector<int>>& adj, int node, vector<bool>& visited) {
         visited[node] = true;
         
-        for (int i = 0; i < adj[node].size(); i++){
-            if (!visited[adj[node][i]]){
+        for (int i = 0; i < adj[node].size(); i++) {
+            if (!visited[adj[node][i]]) {
                 dfs(adj, adj[node][i], visited);
             }
         }        
@@ -18,12 +18,12 @@ public:
         vector<bool> visited(n, false);
         int ans = 0;
         
-        for (int i = 0; i < edges.size(); i++){
+        for (int i = 0; i < edges.size(); i++) {
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
         
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 ans++;
                 dfs(adj, i, visited);
