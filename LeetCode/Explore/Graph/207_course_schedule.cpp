0b3e3d02d879cf14e52bdbb44c4ca70a1a46bddb
Mindy Jen <jencmhep@gmail.com>
2022-04-14 11:37:49 -0400

    update_220414

--- LeetCode/Explore/Graph/207_course_schedule.cpp
+++ LeetCode/Explore/Graph/207_course_schedule.cpp
@@ -1,6 +1,43 @@
 class Solution {
   
 private:
+  bool dfs(int cur, vector<int> &visited, vector<vector<int> > &adj) { // is cycle detected? 
+    // yes, return true
+    // no, return false
+    if (visited[cur] == 2) {
+      return true;
+    }
+    visited[cur] = 2;
+    for (auto &neighbor : adj[cur]) {
+      if (visited[neighbor] != 1 && dfs(neighbor, visited, adj)) {
+        return true;
+      }
+    }
+    visited[cur] = 1;
+    return false;
+  }
+  
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+      vector<int> visited(numCourses, 0);
+      vector<vector<int> > adj(numCourses);
+      for (auto &prerequisite : prerequisites) {
+        adj[prerequisite[0]].push_back(prerequisite[1]);
+      }
+      for (int i = 0; i < numCourses; i++) {
+        if (!visited[i]) {
+          if (dfs(i, visited, adj)) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+};
+
+class Solution {
+  
+private:
   bool dfs(int curr, vector<int> &visited, vector<vector<int> > &adj) {
     if (visited[curr] == 2) {
       return true;
