e77a14f265960e9e73dfcb110d4ff93e47310c5f
Mindy Jen <jencmhep@gmail.com>
2022-04-14 14:12:49 -0400

    update_220414

--- LeetCode/Explore/Graph/323_number_of_connected_components_in_an_undirected_graph.cpp
+++ LeetCode/Explore/Graph/323_number_of_connected_components_in_an_undirected_graph.cpp
@@ -1,4 +1,43 @@
 class Solution {
+
+private:
+  //convert the graph into adjacency list first 
+  //number of dfs is the number of components  
+    void dfs(int cur, vector<bool>& visited, vector<vector<int> > &adj) { // is cycle detected?
+      // yes, return true
+      // no, return false
+      visited[cur] = true;
+      
+      for (auto &neighbor : adj[cur]) {
+        if (!visited[neighbor]) {
+          dfs(neighbor, visited, adj);
+        }
+      }   
+    }
+  
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+      vector<bool> visited(n, false);
+      vector<vector<int> > adj(n);
+      int ans = 0;
+      
+      for(auto &edge : edges) {
+        adj[edge[0]].push_back(edge[1]);
+        adj[edge[1]].push_back(edge[0]);
+      }
+        
+      for(int i = 0; i < n; i++) {
+        if (!visited[i]) {
+          ans++;
+          dfs(i, visited, adj);
+        }            
+      }
+      return ans;
+    }
+};
+
+
+class Solution {
 public:
     //convert the graph into adjacency list first 
     //number of dfs is the number of components
