2ea353b86fb49df52e5dc37513a5abc0bfe9caa9
Mindy Jen <jencmhep@gmail.com>
2022-04-24 18:51:23 -0400

    update_220424

---
+++ LeetCode/Explore/TopInterview/Meta/SortingSearching/34_find_first_and_last_position_of_element_in_sorted_array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+      int pivot, l = 0, r = nums.size() - 1;
+      while (l <= r) {
+        pivot = l + (r - l) / 2;
+        if (nums[pivot] < target) {
+          l = pivot + 1;
+        }
+        else if (nums[pivot] > target) {
+          r = pivot - 1;
+        }
+        else {
+          r = pivot;
+          l = pivot;
+          while (r < nums.size() && nums[r] == target) {
+            r++;
+          }
+          while (l >= 0 && nums[l] == target) {
+            l--;
+          }
+          return {l + 1, r - 1};
+        }
+      }
+      return {-1, -1};
+    }
+};
