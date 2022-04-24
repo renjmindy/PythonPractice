f17cdef39580a1528d1704217847a8738da451a6
Mindy Jen <jencmhep@gmail.com>
2022-04-24 18:16:39 -0400

    update_220424

---
+++ LeetCode/Explore/TopInterview/Meta/SortingSearching/33_search_in_rotated_sorted_array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+      int pivot, l = 0, r = nums.size() - 1; 
+      while (l <= r) {
+        pivot = l + (r - l) / 2;
+        if (nums[pivot] == target) {
+          return pivot;
+        }
+        else if (nums[pivot] >= nums[l]) { // end points
+          if (target >= nums[l] && target < nums[pivot]) {
+            r = pivot - 1;
+          }
+          else {
+            l = pivot + 1;
+          }
+        }
+        else {
+          if (target > nums[pivot] && target <= nums[r]) { // end points
+            l = pivot + 1;
+          }
+          else {
+            r = pivot - 1;
+          }
+        }
+      }
+      return -1;
+    }
+};
