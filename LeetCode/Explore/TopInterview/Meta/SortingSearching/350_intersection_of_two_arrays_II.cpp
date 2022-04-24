dd8c7c17b213f98b9d78b7f27f339db3d1f89deb
Mindy Jen <jencmhep@gmail.com>
2022-04-24 15:48:15 -0400

    update_220424

---
+++ LeetCode/Explore/TopInterview/Meta/SortingSearching/350_intersection_of_two_arrays_II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+      unordered_map<int, int> mp1, mp2;
+      vector<int> ans;
+      for (auto &num : nums1) {
+        mp1[num]++;
+      }
+      for (auto &num : nums2) {
+        if (mp1.count(num) && mp2[num] != mp1[num]) {
+          mp2[num]++;
+        }
+      }
+      for (auto &m : mp2) {
+        for (int i = 0; i < m.second; i++) {
+          ans.push_back(m.first);
+        }
+      }
+      return ans;
+    }
+};
