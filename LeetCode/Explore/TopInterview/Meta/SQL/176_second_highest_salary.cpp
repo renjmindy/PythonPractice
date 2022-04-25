bcf61a236b935cd1b7423c0be91ded75878309c6
Mindy Jen <jencmhep@gmail.com>
2022-04-25 12:02:59 -0400

    Delete 176_second_highest_salary.cpp

--- LeetCode/Explore/TopInterview/Meta/SQL/176_second_highest_salary.cpp
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-WITH CTE AS (SELECT *, DENSE_RANK() OVER (ORDER BY salary DESC) AS ranking FROM Employee)
-SELECT MAX(salary) AS SecondHighestSalary
-FROM CTE
-WHERE ranking = 2
