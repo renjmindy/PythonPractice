5fad257f760269417c1edede7acd81d235cc67c8
Mindy Jen <jencmhep@gmail.com>
2022-04-25 17:49:46 -0400

    update_220425

---
+++ LeetCode/Explore/TopInterview/Google/ArrayString/3_longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+      int ans = 0;
+      unordered_map<int, int> mp;
+      for (int r = 0, l = 0; r < s.size(); r++) {
+        if (mp.count(s[r])) {
+          l = max(l, mp[s[r]]);
+        }
+        ans = max(ans, r - l + 1);
+        mp[s[r]] = r + 1;
+      }
+      return ans;
+    }
+};
