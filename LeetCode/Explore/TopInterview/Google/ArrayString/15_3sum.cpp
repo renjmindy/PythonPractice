4f35468a013ba77c0c08b569c0871aa323abe78a
Mindy Jen <jencmhep@gmail.com>
2022-04-25 21:26:01 -0400

    update_220425

---
+++ LeetCode/Explore/TopInterview/Google/ArrayString/15_3sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  
+private:
+  vector<vector<int> > ans;
+  void twoSum(int i, vector<int> &nums, vector<vector<int> > &ans) {
+    int l = i + 1, r = nums.size() - 1;
+    while (l < r) {
+      int sum = nums[i] + nums[l] + nums[r];
+      if (sum > 0) {
+        r--;
+      }
+      else if (sum < 0) {
+        l++;
+      }
+      else {
+        ans.push_back({nums[i], nums[l], nums[r]});
+        l++;
+        r--;
+        while (l < r && nums[l - 1] == nums[l]) {
+          l++;
+        }
+      }
+    }
+  }
+  
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+      sort(nums.begin(), nums.end());
+      for (int i = 0; i < nums.size() && nums[i] <= 0; i++) {
+        if (ans.empty() || nums[i - 1] != nums[i]) {
+          twoSum(i, nums, ans);
+        }
+      }
+      return ans;
+    }
+};
