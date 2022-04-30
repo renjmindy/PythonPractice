35e99af8c873111b57797b618abb9bb8bc6817e7
Mindy Jen <jencmhep@gmail.com>
2022-04-30 14:36:50 -0400

    update_220430

---
+++ LeetCode/Explore/TopInterview/Google/ArrayString/159_longest_substring_with_at_most_two_distinct_characters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  
+private:
+  int lengthOfLongestSubstringKDistinct(string s, int k) {
+    if (s.size() <= k) {
+      return s.size();
+    }
+    unordered_map<char, int> mp;
+    int maxLen = k;
+    for (int r = 0, l = 0; r < s.size(); r++) {
+      mp[s[r]] = r;
+      if (mp.size() == k + 1) {
+        int idx = s.size() - 1;
+        for (auto &m : mp) {
+          idx = min(idx, m.second);
+        }
+        mp.erase(s[idx]);
+        l = idx + 1;
+      }
+      maxLen = max(maxLen, r - l + 1);
+    }
+    return maxLen;
+  }
+  
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+      return lengthOfLongestSubstringKDistinct(s, 2);
+    }
+};
