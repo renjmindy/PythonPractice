11ef7d6bada68ad15f232bfa496449f5533f7d2f
Mindy Jen <jencmhep@gmail.com>
2022-05-01 15:59:41 -0400

    update_220501

--- LeetCode/Explore/TopInterview/Meta/SQL/197_rising_temperature.sql
+++ LeetCode/Explore/TopInterview/Meta/SQL/197_rising_temperature.sql
@@ -1,1 +1,11 @@
+# Write your MySQL query statement below
+SELECT DISTINCT w2.id AS id 
+FROM Weather AS w1
+JOIN Weather AS w2
+ON DATEDIFF(w2.recordDate, w1.recordDate) = 1 
+WHERE w2.temperature > w1.temperature
 
+# Write your MySQL query statement below
+SELECT DISTINCT w2.id AS id 
+FROM Weather AS w1, Weather AS w2
+WHERE DATEDIFF(w2.recordDate, w1.recordDate) = 1 AND w2.temperature > w1.temperature
