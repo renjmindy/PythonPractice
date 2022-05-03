f07fd6aab434fffb931b5b75d8557c98252c7777
Mindy Jen <jencmhep@gmail.com>
2022-05-03 13:20:42 -0400

    update_220503

---
+++ LeetCode/Explore/TopInterview/Google/LinkedList/138_copy_list_with_random_pointer.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+  private:
+    unordered_map<Node*, Node*> mp;
+  public:
+    Node* copyRandomList(Node* head) {
+      if (!head) {
+        return head;
+      }
+      if (mp.count(head)) {
+        return mp[head];
+      }
+      else {
+        mp[head] = new Node(head->val);
+        mp[head]->next = copyRandomList(head->next);
+        mp[head]->random = copyRandomList(head->random);
+        return mp[head];
+      }
+    }
+};
